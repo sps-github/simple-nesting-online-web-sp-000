1064a79f700bd0e9f13ad415cb41149914aaa4e7
Sarah Peters <sarahpieters15@gmail.com>
2020-06-17 13:11:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,10 +74,6 @@ programmer_hash << [:yukihiro_matsumoto => {
 
 	programmer_hash =
  		{
-			:yukihiro_matsumoto => {
-				:known_for => "Ruby",
-				:languages => ["LISP", "C"]
-			}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
